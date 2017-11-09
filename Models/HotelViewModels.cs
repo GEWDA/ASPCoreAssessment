4b16ee1823ae736d7ff1c247afec480cb586d15c
Judah Parsons <GEWDDEWD9@hotmail.com>
2017-11-09 14:21:33 +1300

    Made viewmodels

---
+++ Models/HotelViewModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCoreAssessment.Models
+{
+    public class HotelViewModelRoom
+    {
+        public IEnumerable<HotelRoom> Rooms { get; set; }
+    }
+    public class HotelViewModelBooking
+    {
+        public IEnumerable<HotelBooking> Bookings { get; set; }
+    }
+    public class HotelViewModelGuest
+    {
+        public IEnumerable<HotelGuest> Guests { get; set; }
+    }
+    public class HotelViewModelBill
+    {
+        public IEnumerable<HotelBill> Billing { get; set; }
+    }
+}
